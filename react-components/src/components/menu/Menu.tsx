1f195b5190c87819d648158e516a6f0d256a9d27
skif-cezar <skif.cezar@gmail.com>
2022-07-13 17:02:08 +0300

    fix: activ item in menu to fix bugs

--- react-components/src/components/menu/Menu.tsx
+++ react-components/src/components/menu/Menu.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Menu.css';
 
 const Menu = () => {
   return (
     <>
       <nav className="header__navigation">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/404">404</Link>
+        <NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          About Us
+        </NavLink>
+        <NavLink to="/404" className={({ isActive }) => (isActive ? 'active-link' : '')}>
+          404
+        </NavLink>
       </nav>
     </>
   );
